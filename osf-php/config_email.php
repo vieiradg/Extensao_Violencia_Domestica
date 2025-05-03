208624d37495169640605a21efad552d44cdd52d
osf-localhost <ixunda@hotmail.com>
2025-05-03 00:11:44 -0300

    Add files via upload

--- osf-php/config_email.php
+++ osf-php/config_email.php
@@ -5,7 +5,7 @@ if (basename(__FILE__) == basename($_SERVER["SCRIPT_FILENAME"])) {
     exit("Acesso negado.");
 }
 
-$email_smtp = 'ixunda.if@gmail.com';          // G-mail que irá enviar o formulario de contato.
-$senha_smtp = 'cfqmaehxfdqojnqs';            // Senha aplicativo do Gmail que irá enviar o formulario de contato.
-$destinatario = 'ixunda.if@gmail.com';        // E-mail que vai receber o formulario de contato!
+$email_smtp = 'seugmail@gmail.com';          // G-mail que irá enviar o formulario de contato.
+$senha_smtp = 'senha_aplicativo';            // Senha aplicativo do Gmail que irá enviar o formulario de contato.
+$destinatario = 'email_destino@gmail.com';        // E-mail que vai receber o formulario de contato!
 ?>
